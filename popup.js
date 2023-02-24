4eca08abb91755893e533d7dcb0df8d5390bb248
nebratna <122659598+nebratna@users.noreply.github.com>
2023-02-24 15:12:35 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,2 +1,2 @@
-var msg = "Red points are places I hiked/visited. You can click on each to obtain additional information. Once zoomed close enough provincial parks of Ontario (protected areas) would appear shaded in purple; you can click on each to learn its name and class.";
+var msg = "Red points are places I hiked/visited. You can click on each to obtain additional information. Once zoomed close enough provincial parks of Ontario (protected areas) will appear shaded in purple; you can click on each to learn its name and class.";
 alert(msg);
