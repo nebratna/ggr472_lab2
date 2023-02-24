5cfb68a9e5a224c048a82e5c1d2ad28920cc1476
nebratna <122659598+nebratna@users.noreply.github.com>
2023-02-23 22:07:28 -0500

    added places-hiked-and-visited layer

--- map_script.js
+++ map_script.js
@@ -1,48 +1,35 @@
-//Code to display my map on a webpage
-mapboxgl.accessToken = 'pk.eyJ1IjoibmVicmF0bmEiLCJhIjoiY2xjdmZ6Z3I0MDdoODNycWtvNDVuYjJydCJ9.MU8-uPe3u6ya0aTiMr079g'; //default public map token from my Mapbox account 
+//Define access tocken
+mapboxgl.accessToken = 'pk.eyJ1IjoibmVicmF0bmEiLCJhIjoiY2xjdmZ6Z3I0MDdoODNycWtvNDVuYjJydCJ9.MU8-uPe3u6ya0aTiMr079g'; //default public map token from my Mapbox account
 
+//Initialize map
 const map = new mapboxgl.Map({
     container: 'map', // container ID for my map
     style: 'mapbox://styles/nebratna/clehfnjkh001501nyd6k3pq0a', // URL link to my Mapbox Studio style basemap
-    center: [-79.35, 43.65], // starting position [longitude, latitude]
-    zoom: 9, // starting zoom
+    center: [-84.475641, 48.012494], // starting position [longitude, latitude]
+    zoom: 6, // starting zoom
 });
 
+//After map load event, add data sources and draw layers
 map.on('load', () => {
-    
-    /*Adding a source for GeoJSON*/
-    map.addSource('locations', { // my source ID for points I visited/hiked
-        type: 'geojson',
-        data: 'https://github.com/nebratna/ggr472_lab2/blob/main/Data/RoadTrip2021.geojson'
-    });
-
+    console.log('A load event occured'); //to check in Developer Console if it loads
     /*Adding a source for Mapbox Tileset*/
-    map.addSource('ONparks', { // my source ID for ON parks
+    map.addSource('visits', { // my source ID for places visited/hiked
         type: 'vector',
-        url: 'mapbox://nebratna.cewul63m' //my tileset link from Mapbox
+        url: 'mapbox://nebratna.b1z3476g' //my tilesetID link from Mapbox
     });
-    
-    /*Drawing geometry for GeoJSON as circles*/
+
+    /*Drawing geometry for tileset data as points*/
     map.addLayer({
-        'id': 'visited-places',
+        'id': 'places-hiked-and-visited',
+        'source': 'visits',//matches my source ID in addSource method above
+        'source-layer': 'RoadTrip2021-4scneg',
         'type': 'circle',
-        'source': 'locations',
         'paint': {
-            'circle-radius': 5,
-            'circle-color': 'blue'
+            'circle-color': 'blue',
+            'circle-radius': 8,
         },
     });
 
-    /*Drawing geometry for tileset data as polygons*/
-    map.addLayer({
-        'id': 'parks',
-        'type': 'fill',
-        'source': 'ONparks', //matches my source ID in addSource method above
-        'paint': {
-            'fill-color': 'green',
-            'fill-opacity': 0.9,
-            'fill-outline-color': 'blue'
-        },
-        'source-layer': 'Provincial_park_regulated-d0gx9s' // name of my tileset layer from Mapbox tilesets page
-    });
+
+
 });
