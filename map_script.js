86073553d7a5b9967b55072225871a79ccdd3545
nebratna <122659598+nebratna@users.noreply.github.com>
2023-02-24 00:01:50 -0500

    added a mouse click event for places-hiked-and-visited

--- map_script.js
+++ map_script.js
@@ -5,9 +5,11 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibmVicmF0bmEiLCJhIjoiY2xjdmZ6Z3I0MDdoODNycWtvN
 const map = new mapboxgl.Map({
     container: 'map', // container ID for my map
     style: 'mapbox://styles/nebratna/clehqd5k6002601nre3suucxz', // URL link to my Mapbox Studio style basemap
-    center: [-84.475641, 48.012494], // starting position [longitude, latitude]
-    zoom: 6, // starting zoom
+    center: [-84.67401692764999, 46.958721756061216], // starting position [longitude, latitude]
+    zoom: 6.5, // starting zoom
 });
+
+
 
 //After map load event, add data sources and draw layers
 map.on('load', () => {
@@ -18,7 +20,7 @@ map.on('load', () => {
         url: 'mapbox://nebratna.b1z3476g' //my tilesetID link from Mapbox
     });
 
-    map.addSource('parks-Ontario', { //set-up source ID for ONparks
+    map.addSource('parks-Ontario', { //set-up source ID for ONparks, only z7-22
         type: 'vector',
         url: 'mapbox://nebratna.cewul63m' // my tilesetID from Mapbox Studio
     });
@@ -33,8 +35,10 @@ map.on('load', () => {
             'circle-color': 'blue',
             'circle-radius': 8,
         },
+        'minzoom': 6.5,
+
     });
-   
+
     /*Drawing geometry for tileset data as polygons*/
     map.addLayer({
         'id': 'ONparks',
@@ -48,4 +52,27 @@ map.on('load', () => {
         },
     });
 
+    /*Add a mouse click event*/
+
+    // Change the cursor to a pointer when the mouse is over the places-hiked-and-visited layer.
+    map.on('mouseenter', 'places-hiked-and-visited', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'places-hiked-and-visited', () => {
+        map.getCanvas().style.cursor = '';
+    });
+
+    map.on('click', 'places-hiked-and-visited', (e) => {
+        new mapboxgl.Popup() //Declare new popup object on each click
+            .setLngLat(e.lngLat) //Use method to set coordinates of popup based on mouse click location
+            .setHTML("<b>Hiked/Visited:</b> " + (e.features[0].properties.hikes || e.features[0].properties.visited) + "<br>" + "<b>Date:</b> " + e.features[0].properties.date) //Use click event properties to write text for popup
+            .addTo(map); //Show  popup on map
+    });
+
 });
+
+
+
+
