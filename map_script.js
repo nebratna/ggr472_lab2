b2940f44adddc5e536687b5e976c78de826a378b
nebratna <122659598+nebratna@users.noreply.github.com>
2023-02-23 22:36:15 -0500

    both layers added to basemap-1

--- map_script.js
+++ map_script.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibmVicmF0bmEiLCJhIjoiY2xjdmZ6Z3I0MDdoODNycWtvN
 //Initialize map
 const map = new mapboxgl.Map({
     container: 'map', // container ID for my map
-    style: 'mapbox://styles/nebratna/clehfnjkh001501nyd6k3pq0a', // URL link to my Mapbox Studio style basemap
+    style: 'mapbox://styles/nebratna/clehqd5k6002601nre3suucxz', // URL link to my Mapbox Studio style basemap
     center: [-84.475641, 48.012494], // starting position [longitude, latitude]
     zoom: 6, // starting zoom
 });
@@ -13,9 +13,14 @@ const map = new mapboxgl.Map({
 map.on('load', () => {
     console.log('A load event occured'); //to check in Developer Console if it loads
     /*Adding a source for Mapbox Tileset*/
-    map.addSource('visits', { // my source ID for places visited/hiked
+    map.addSource('visits', { // set-up source ID for places-hiked-and-visited
         type: 'vector',
         url: 'mapbox://nebratna.b1z3476g' //my tilesetID link from Mapbox
+    });
+
+    map.addSource('parks-Ontario', { //set-up source ID for ONparks
+        type: 'vector',
+        url: 'mapbox://nebratna.cewul63m' // my tilesetID from Mapbox Studio
     });
 
     /*Drawing geometry for tileset data as points*/
@@ -29,7 +34,18 @@ map.on('load', () => {
             'circle-radius': 8,
         },
     });
-
-
+   
+    /*Drawing geometry for tileset data as polygons*/
+    map.addLayer({
+        'id': 'ONparks',
+        'source': 'parks-Ontario',
+        'source-layer': 'Provincial_park_regulated-d0gx9s',
+        'type': 'fill',
+        'paint': {
+            'fill-color': 'black',
+            'fill-opacity': 0.4,
+            'fill-outline-color': 'yellow'
+        },
+    });
 
 });
